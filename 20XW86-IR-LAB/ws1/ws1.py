baf73749dc3998deafa1e25494b24e2acf287c8c
unknown <20PW16@cs2k16.amcs.ws2k12.psgcs.edu>
2023-12-21 09:42:29 +0530

    Add data viz.

--- 20XW86-IR-LAB/ws1/ws1.py
+++ 20XW86-IR-LAB/ws1/ws1.py
@@ -14,6 +14,8 @@ site1 = requests.get("https://en.wikipedia.org/wiki/List_of_Nobel_laureates")
 soup = BeautifulSoup(site1.text, 'html.parser')
 # print(soup)
 mydiv = soup.find("table")
+# t_rows = soup.find_all("tr")
+# print(t_rows)
 
 file = open("wiki1.html","w",encoding="utf-8")
 file.write(str(mydiv))
